2de8ee1c499470b76abcad172beab9eac7c1f91a
Rony <rony.sant20@gmail.com>
2021-12-09 20:36:22 -0600

    update about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,9 +5,9 @@ import Navbar from "../Components/Navbar";
 
 export default function About(props) {
   return (
-    <>
+    <div className="App">
       <Navbar />
       <h1>About</h1>
-    </>
+    </div>
   );
 }
